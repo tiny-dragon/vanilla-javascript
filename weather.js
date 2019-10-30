403fd65326941323e55b2caef77904667a5aa914
wangjin8855 <wangjin8855@hotmail.com>
2019-10-30 10:00:00 +0000

    added Weather API call function

--- weather.js
+++ weather.js
@@ -1,7 +1,35 @@
+const weather = document.querySelector('.js-weather')
+
+const API_KEY = '8766ff947ed713c276d8e7ea6cd4eeb0'
 const COORDS_LS = 'coords'
 
-function handleGeoSuccess(position) {
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temparature = json.main.temp
+        const place = json.name
+        weather.innerText = `${temparature} @ ${place}`
+    })
+}
 
+function saveCoods(coodsObj) {
+    localStorage.setItem(COORDS_LS, JSON.stringify(coodsObj))
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const coodsObj = {
+        latitude,
+        longitude
+    }
+
+    saveCoods(coodsObj)
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError(error) {
@@ -18,7 +46,8 @@ function loadCoords() {
     if (loadedCoords === null) {
         askForCoods()
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords)
+        getWeather(parsedCoords.latitude, parsedCoords.longitude)
     }
 }
 
